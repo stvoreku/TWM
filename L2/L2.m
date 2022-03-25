37b4c7821fd8750f74550390f80431f61190492d
jkulawik <darkfire.9251@gmail.com>
2022-03-25 17:32:07 +0100

    drobne poprawki

--- L2/L2.m
+++ L2/L2.m
@@ -1,7 +1,6 @@
 orgImg = imread("IMG_5772.jpg");
 
-
-% Blok tworz±cy i poprawiaj±cy maske
+% -- Tworzenie i filtracja maski -- %
 [BW1, imge1] = createMaskColorful(orgImg);
 [BW2, imge2] = createMaskBright2(orgImg);
 BW3 = logical((1 - BW1) + (1 - BW2));
@@ -10,19 +9,20 @@ BW3 = logical((1 - BW1) + (1 - BW2));
 
 [BW5, properties] = filterRegions(BW4);
 
-% izolacja poszczegolnych skladowych przestrzeni HSV
+% -- Izolacja skladowych obrazu w przestrzeni HSV --%
 HSV = rgb2hsv(orgImg);
 H = HSV(:,:,1);
 S = HSV(:,:,2);
 V = HSV(:,:,3);
 
-% detekcja rejonow
+% -- Detekcja rejonow -- %
 props = regionprops(BW5, {"MajorAxisLength", "MinorAxisLength", 'Area', 'Eccentricity', 'EquivDiameter', 'BoundingBox', 'PixelIdxList', 'Centroid'});
 
-%detekcja koloru i ksztaltu
+% -- Oznaczanie obiektów -- %
 out = orgImg;
 for i=1:size(properties, 1)
     if props(i).Area > 500
+
         avg_h = mean(H(props(i).PixelIdxList));
         avg_s = mean(S(props(i).PixelIdxList));
         avg_v = mean(V(props(i).PixelIdxList));
@@ -31,43 +31,42 @@ for i=1:size(properties, 1)
         labelS = sprintf("%.3f", avg_s);
         labelV = sprintf("%.3f", avg_v);
         
-        color = hsv2rgb([avg_h, 1, 1]) * 255;
-        
+        % -- Oznaczenie koloru -- %
         if avg_s < 0.4
             if avg_v > 0.75
-                label = " White";
+                color_l = " White";
             elseif avg_v > 0.4
-                label = " Gray";
+                color_l = " Gray";
             else
-                label = " Black";
+                color_l = " Black";
             end
         else
             if avg_h < 0.1
-                label = " Red";
+                color_l = " Red";
             elseif (avg_h > 0.1) & (avg_h < 0.20)
-                label = " Yellow";
+                color_l = " Yellow";
             elseif (avg_h > 0.2) & (avg_h < 0.45)
-                label = " Green";
+                color_l = " Green";
             elseif (avg_h > 0.45) & (avg_h < 0.75)
-                label = " Blue";
+                color_l = " Blue";
             elseif avg_h > 0.90
-                label = " Pink";
+                color_l = " Pink";
             end
         end
+
+        % -- Oznaczenie kszta³tu -- %
         if props(i).Eccentricity < 0.3
-            shape_l = "circle";
             shape = "circle";
             pos = [props(i).Centroid, props(i).EquivDiameter/2];
         elseif props(i).MajorAxisLength / props(i).MinorAxisLength <= 1.5
-            shape_l = "square";
             shape = "rectangle";
             pos = props(i).BoundingBox;
         else
-            shape_l = "rectangle";
             shape = "rectangle";
             pos = props(i).BoundingBox;
         end
-        out = insertObjectAnnotation(out, shape, pos, shape_l+ ' ' +label+ 'H:'+labelH+'S:'+labelS+'V:'+labelV) ;
+        label = color_l+' '+shape+' '+ 'H:'+labelH+'S:'+labelS+'V:'+labelV;
+        out = insertObjectAnnotation(out, shape, pos, label);
     end
 end
 imshow(out);
