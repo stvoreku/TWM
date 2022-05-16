875ecdcc17f322135d8cfd89c76d2b697795a2b5
Kuba <kuba@fedora.laptop>
2022-05-16 21:02:22 +0200

    konwersja obrazow z PPM na PNG

--- Projekt/GTSRB/Training/convert_ppm.sh
+++ Projekt/GTSRB/Training/convert_ppm.sh
@@ -1,4 +1,4 @@
 # Pierwsze polecenie zmienia *.ppm w PNG o nazwach *.ppm.png
 # Drugie usuwa wszystkie *.ppm
-# find ./test -name *.ppm -type f -exec convert {} {}.png \;
-# find ./test -name *.ppm -type f -exec rm {} \;
+# find . -name *.ppm -type f -exec convert {} {}.png \;
+find . -name *.ppm -type f -exec rm {} \;
