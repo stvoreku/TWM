cc39e09be140441d502925ea7073c2a3a8afcb4c
jkulawik <darkfire.9251@gmail.com>
2022-05-24 14:40:09 +0200

    Misc progress and requirements

--- Projekt/loadDataset.py
+++ Projekt/loadDataset.py
@@ -5,10 +5,15 @@ import random                        # for generating random numbers
 from tensorflow import keras
 
 from keras.models import Sequential  # Model type to be used
-from keras.layers.core import Dense, Dropout, Activation  # Types of layers to be used in our model
+from keras import layers
 
-from keras.utils import np_utils                         # NumPy related tools
+from keras.utils import np_utils  # NumPy related tools
 from keras import optimizers
+
+# ----------------- Config ----------------- #
+img_height = 256
+img_width = 256
+internal_img_size = (img_height, img_width)
 
 
 def load_dataset(subset="training"):
@@ -18,7 +23,7 @@ def load_dataset(subset="training"):
         label_mode="int",
         color_mode="grayscale",
         batch_size=32,
-        image_size=(256, 256),  # to skalowanie chyba niestety jest obowiązkowe
+        image_size=internal_img_size,  # skalowanie obrazu, niestety jest chyba obowiązkowe
         interpolation="bilinear",
         shuffle=True,
         validation_split=0.99,  # Optional float between 0 and 1, fraction of data to reserve for validation
@@ -28,14 +33,16 @@ def load_dataset(subset="training"):
     )
 
 
+# ----------------- Main ----------------- #
+# https://www.tensorflow.org/tutorials/images/classification
+
 train_dataset = load_dataset()
 # valid_dataset = load_dataset("validation")
-
-# https://www.tensorflow.org/tutorials/images/classification
 
 class_names = train_dataset.class_names
 print(class_names)
 
+# Ilustracja zbioru, 9 obrazów
 plt.figure(figsize=(10, 10))
 for images, labels in train_dataset.take(1):
     for i in range(9):
@@ -45,4 +52,20 @@ for images, labels in train_dataset.take(1):
         plt.axis("off")
 plt.show()
 
+
+# Przykładowa sieć z artykułu
+num_classes = len(class_names)
+
+model = Sequential([
+  layers.Rescaling(1./255, input_shape=(img_height, img_width, 3)),
+  layers.Conv2D(16, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(32, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Conv2D(64, 3, padding='same', activation='relu'),
+  layers.MaxPooling2D(),
+  layers.Flatten(),
+  layers.Dense(128, activation='relu'),
+  layers.Dense(num_classes)
+])
 
