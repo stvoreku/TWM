ac56b00ebf6ec59fc59081ef8d84664b2b43be40
jkulawik <darkfire.9251@gmail.com>
2022-05-24 14:31:45 +0200

    Fixed indentation

--- Projekt/loadDataset.py
+++ Projekt/loadDataset.py
@@ -38,11 +38,11 @@ print(class_names)
 
 plt.figure(figsize=(10, 10))
 for images, labels in train_dataset.take(1):
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(class_names[labels[i]])
+        plt.axis("off")
 plt.show()
 
 
