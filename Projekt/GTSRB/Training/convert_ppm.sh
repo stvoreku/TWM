c821590fbbb3fa6b754546a1c437ac9cb80b156c
Kuba <kuba@fedora.laptop>
2022-05-26 22:05:47 +0200

    konwersja drugiego datasetu

--- Projekt/GTSRB/Training/convert_ppm.sh
+++
@@ -1,4 +0,0 @@
-# Pierwsze polecenie zmienia *.ppm w PNG o nazwach *.ppm.png
-# Drugie usuwa wszystkie *.ppm
-# find . -name *.ppm -type f -exec convert {} {}.png \;
-find . -name *.ppm -type f -exec rm {} \;
