ce6a49528c7a577eb80d67b0f84d457a2b42575f
jkulawik <darkfire.9251@gmail.com>
2022-06-14 17:08:35 +0200

    drobne poprawki stylu

--- Projekt/extractor2.py
+++ Projekt/extractor2.py
@@ -3,6 +3,7 @@ import numpy as np
 
 sc = 0.8
 
+
 def extract_blue(img):
 	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
@@ -11,7 +12,7 @@ def extract_blue(img):
 
 	mask = cv2.inRange(hsv_img, lower_blue, upper_blue)
 
-	cv2.imshow("Mask blue", mask)
+	# cv2.imshow("Mask blue", mask)
 
 	kernel = np.ones((10, 10), np.uint8)
 
@@ -25,7 +26,7 @@ def extract_blue(img):
 
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-	cv2.imshow("Mask blue past morph", mask)
+	# cv2.imshow("Mask blue past morph", mask)
 
 	kernel = np.ones((20, 20), np.uint8)
 
@@ -41,22 +42,22 @@ def extract_blue(img):
 		cX = int(M["m10"] / M["m00"])
 		cY = int(M["m01"] / M["m00"])
 		x, y, w, h = cv2.boundingRect(c)
-		if w>h:
-			#print(w / h)
-			w=h
-
+		if w > h:
+			# print(w / h)
+			w = h
 		else:
-			#print(h / w)
-			h=w
-		#cv2.rectangle(img, (cX - int(0.8*h), cY - int(0.8*w)), (cX + int(0.8*w), cY + int(0.8*h)), (0, 255, 0), 2)
+			# print(h / w)
+			h = w
+		# cv2.rectangle(img, (cX - int(0.8*h), cY - int(0.8*w)), (cX + int(0.8*w), cY + int(0.8*h)), (0, 255, 0), 2)
 		cropped_image = img[cY - int(sc*w):cY + int(sc*w), cX - int(sc*h):(cX + int(sc*w))]
 		try:
-			resized_image = cv2.resize(cropped_image, (128,128))
+			resized_image = cv2.resize(cropped_image, (128, 128))
 			extracted.append(resized_image)
 			regions.append((cX - int(sc*h), cY - int(sc*w), cX + int(sc*h), cY + int(sc*w)))
 		except:
 			pass
 	return extracted, regions
+
 
 def extract_red(img):
 	hsv_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
@@ -68,7 +69,7 @@ def extract_red(img):
 	mask_2 = cv2.inRange(hsv_img, lower_red_2, upper_red_2)
 	mask = cv2.bitwise_or(mask_1, mask_2)
 
-	cv2.imshow("Mask red", mask)
+	# cv2.imshow("Mask red", mask)
 
 	kernel = np.ones((10, 10), np.uint8)
 
@@ -82,14 +83,12 @@ def extract_red(img):
 
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-
-
 	kernel = np.ones((20, 20), np.uint8)
 
 	mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
 	mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
-	cv2.imshow("Mask red past morph", mask)
+	# cv2.imshow("Mask red past morph", mask)
 
 	cnts, hier = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 	extracted = []
@@ -100,23 +99,21 @@ def extract_red(img):
 		cX = int(M["m10"] / M["m00"])
 		cY = int(M["m01"] / M["m00"])
 		x, y, w, h = cv2.boundingRect(c)
-		if w>h:
+		if w > h:
 			print(w / h)
-			w=h
-
+			w = h
 		else:
 			print(h / w)
-			h=w
+			h = w
 
-		#cv2.rectangle(img, (cX - int(0.8*h), cY - int(0.8*w)), (cX + int(0.8*w), cY + int(0.8*h)), (0, 255, 0), 2)
+		# cv2.rectangle(img, (cX - int(0.8*h), cY - int(0.8*w)), (cX + int(0.8*w), cY + int(0.8*h)), (0, 255, 0), 2)
 		cropped_image = img[cY - int(sc*w):cY + int(sc*w), cX - int(sc*h):(cX + int(sc*w))]
 		try:
-			resized_image = cv2.resize(cropped_image, (128,128))
+			resized_image = cv2.resize(cropped_image, (128, 128))
 			extracted.append(resized_image)
 			regions.append((cX - int(sc*h), cY - int(sc*w), cX + int(sc*h), cY + int(sc*w)))
 		except:
 			pass
-
 
 	return extracted, regions
 
